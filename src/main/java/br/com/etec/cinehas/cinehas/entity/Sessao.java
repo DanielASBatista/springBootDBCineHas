b3e91ecfa70518f2f7ae6a408aadaf8cdaea2936
DanielASBatista <daniel_alves.santos@hotmail.com>
2025-10-02 22:25:34 -0300

    Inserção de Sessao e assento.

---
+++ src/main/java/br/com/etec/cinehas/cinehas/entity/Sessao.java
@@ -0,0 +1,30 @@
+package br.com.etec.cinehas.cinehas.entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TBL_SESSAO")
+
+public class Sessao {
+    @Id
+    @Column(name = "ID_SESSAO")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "DT_DATA")
+    private LocalDate data;
+    @Column(name = "HR_HORARIO")
+    private LocalTime time;
+    @Column(name = "NR_PRECO")
+    private double preco;
+    @ManyToOne
+    @JoinColumn(name = "ID_SALA")
+    private Sala sala;
+    @OneToOne
+    @JoinColumn(name = "ID_FILME")
+    private Filme filme;
+}
